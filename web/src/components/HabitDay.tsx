746aadce1ddcf426bb30b48a8f07ca10d616c1ba
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-22 20:40:48 -0300

    feat: Popover e progress bar

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -2,8 +2,25 @@
 //   completed: number;
 // }
 
+import * as Popover from "@radix-ui/react-popover";
+import { ProgressBar } from "./ProgressBar";
+
 export function HabitDay() {
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          Popover
+          <span className="font-semibold text-zinc-400">Domingo</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">
+            22/01
+          </span>
+          <ProgressBar progress={25} />
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 }

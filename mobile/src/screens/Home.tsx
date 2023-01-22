5c7d7bc661d4614e96abcb14a0a5d2792b2c6b0a
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-21 22:03:24 -0300

    feat: Home component

---
+++ mobile/src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+import { Header } from "../components/Header";
+
+export function Home() {
+  return (
+    <View className="bg-background flex-1 px-8 pt-16">
+      <Header />
+    </View>
+  );
+}

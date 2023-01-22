7ad32317676ab8b743ae35c9f6387f72c5c91d3c
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-22 11:04:51 -0300

    feat: Criação do componente HabitDay

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,11 +1,28 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
+import { HabitHay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 export function Home() {
   return (
     <View className="bg-background flex-1 px-8 pt-16">
       <Header />
+
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, i) => (
+          <Text
+            key={`${weekDay}-${i}`}
+            className="text-zinc-400 text-xl font-bold text-center mx-1"
+            style={{ width: DAY_SIZE }}
+          >
+            {weekDay}
+          </Text>
+        ))}
+      </View>
+
+      <HabitHay />
     </View>
   );
 }

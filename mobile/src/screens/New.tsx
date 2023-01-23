9a25802b462e6ebe31bb64db57f7f8887aef7d5d
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-22 22:07:46 -0300

    feat: Componente do checkbox

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
 import { View, ScrollView, Text, TextInput } from "react-native";
 import { BackButton } from "../components/BackButton";
+import { Checkbox } from "../components/Checkbox";
+
+const avaiableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
 
 export function New() {
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function handleToggleWeekDay(weekDayIndex: number) {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays((prevState) =>
+        prevState.filter((weekDay) => weekDay !== weekDayIndex)
+      );
+    } else {
+      setWeekDays((prevState) => [...prevState, weekDayIndex]);
+    }
+  }
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <BackButton />
@@ -15,6 +39,18 @@ export function New() {
         </Text>
 
         <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"></TextInput>
+
+        <Text className="font-semibold mt-4 mb-3 text-white text-base">
+          Qual a recorrência?
+        </Text>
+        {avaiableWeekDays.map((weekDay, i) => (
+          <Checkbox
+            key={weekDay}
+            title={weekDay}
+            checked={weekDays.includes(i)}
+            onPress={() => handleToggleWeekDay(i)}
+          />
+        ))}
       </ScrollView>
     </View>
   );

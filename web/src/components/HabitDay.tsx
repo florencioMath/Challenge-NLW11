5aef084647bd4c799704b649695b8e1298df2e93
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-24 21:51:02 -0300

    feat: Utilizando dados da API no Popover

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -3,6 +3,7 @@ import * as Checkbox from "@radix-ui/react-checkbox";
 import { ProgressBar } from "./ProgressBar";
 import clsx from "clsx";
 import { Check } from "phosphor-react";
+import dayjs from "dayjs";
 
 interface HabitDayProps {
   date: Date;
@@ -10,9 +11,12 @@ interface HabitDayProps {
   amount?: number;
 }
 
-export function HabitDay({ completed = 0, amount = 0 }: HabitDayProps) {
+export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
   const completedPercentage =
     amount > 0 ? Math.round((completed / amount) * 100) : 0;
+
+  const dayAndMonth = dayjs(date).format("DD/MM");
+  const dayOfWeek = dayjs(date).format("dddd");
 
   return (
     <Popover.Root>
@@ -34,9 +38,9 @@ export function HabitDay({ completed = 0, amount = 0 }: HabitDayProps) {
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
           Popover
-          <span className="font-semibold text-zinc-400">Domingo</span>
+          <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">
-            22/01
+            {dayAndMonth}
           </span>
           <ProgressBar progress={completedPercentage} />
           <div className="mt-6 flex flex-col gap-3">

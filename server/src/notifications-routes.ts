b597b0a0e2cc9ae48114ef3c800ee724bb804066
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-01-28 21:32:02 -0300

    feat: Implementação parcial da notificação no web

--- server/src/notifications-routes.ts
+++ server/src/notifications-routes.ts
@@ -1,8 +1,6 @@
 import { FastifyInstance } from "fastify";
 import WebPush from "web-push";
 
-console.log(WebPush.generateVAPIDKeys());
-
 const publicKey =
   "BPowEV6sihsakZihpcHhfvdrDN9xUidDVVybRVGn9Hkkm-Er5hRWAYLMV-zN78FasMzg_7YOD1xht0STeC4xCu0";
 const privateKey = "iwtzvwelGMztV-DjEjWvVHEFt-1XIeL2_nPzbVGaHmc";
@@ -10,15 +8,18 @@ const privateKey = "iwtzvwelGMztV-DjEjWvVHEFt-1XIeL2_nPzbVGaHmc";
 WebPush.setVapidDetails("http://localhost:3333", publicKey, privateKey);
 
 export async function notificationRoutes(app: FastifyInstance) {
-  app.get("/push/publick_key", () => {
+  app.get("/push/public_key", () => {
     return publicKey;
   });
 
-  app.get("/push/register", (request, reply) => {
+  app.post("/push/register", (request, reply) => {
+    console.log(request.body);
+
     return reply.status(201).send();
   });
 
-  app.get("/push/send", async (request, reply) => {
+  app.post("/push/send", async (request, reply) => {
+    console.log(request.body);
     return reply.status(201).send();
   });
 }
